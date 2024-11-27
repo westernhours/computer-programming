3adcced673dd0951f8850b91f7f3392d7ac61e41
Sanvi Jain <jainsanvi907@gmail.com>
2024-11-27 23:45:51 +0530

    list_q5.py

---
+++ coding-assignment/list/list_q5.py
@@ -0,0 +1,6 @@
+#  List Comprehension
+# Given a list numbers = [1, 2, 3, 4, 5], create a new list that contains the squares of each element.
+
+numbers = [1, 2, 3, 4, 5]
+squares = [x**2 for x in numbers]
+print(squares)
